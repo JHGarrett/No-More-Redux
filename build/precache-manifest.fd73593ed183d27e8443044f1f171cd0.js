dc7a21fc6c6eec8dce4c5200d8ab4cfef49b8ef1
John Garrett <infantrygarrett@icloud.com>
2019-07-23 23:42:40 -0500

    refactored code

---
+++ build/precache-manifest.fd73593ed183d27e8443044f1f171cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dceb407ba3cc2a54e1bded7e1fecfab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "009a86bb4b20c4383a65",
+    "url": "/static/css/main.2cd71a98.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "009a86bb4b20c4383a65",
+    "url": "/static/js/main.b5ed9e91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
