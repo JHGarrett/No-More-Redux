d8a9b748b0f373b810a367d8893cf36a24055d91
John Garrett <infantrygarrett@icloud.com>
2019-07-23 21:32:14 -0500

    updated github state

--- src/App.js
+++ src/App.js
@@ -19,19 +19,6 @@ const App = () => {
   const [alert, setAlert] = useState(null);
 
   // search github users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=$
-      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
-        process.env.REACT_APP_GITHUB_CLIENT_SECRET
-      }`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
 
   // ger single user
   const getUser = async username => {
@@ -56,7 +43,7 @@ const App = () => {
         process.env.REACT_APP_GITHUB_CLIENT_SECRET
       }`
     );
-   setRepos(res.data);
+    setRepos(res.data);
     setLoading(false);
   };
 
@@ -86,7 +73,6 @@ const App = () => {
                 render={props => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
